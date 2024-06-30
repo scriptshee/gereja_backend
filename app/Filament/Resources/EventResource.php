5036c8eb3581c77988c0c5fad876121646a7b23f
Arifin <dev.arifin14@gmail.com>
2024-07-01 00:19:27 +0800

    init view record

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -13,6 +13,9 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
 
 class EventResource extends Resource
 {
@@ -51,7 +54,7 @@ class EventResource extends Resource
                         Forms\Components\Checkbox::make('is_endedtime')
                             ->label('Sampai Selesai'),
                         Forms\Components\Select::make('user_id')
-                            ->options(fn() => User::query()->pluck('name', 'id'))
+                            ->options(fn () => User::query()->pluck('name', 'id'))
                             ->default(auth()->id())
                             ->searchable()
                     ])
@@ -72,14 +75,17 @@ class EventResource extends Resource
                     ->default('-'),
                 Tables\Columns\TextColumn::make('user.name')
                     ->label('author'),
-                
+
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -89,6 +95,23 @@ class EventResource extends Resource
             ->striped()
             ->defaultSort('created_at', 'desc');
     }
+
+    // public static function infolist(Infolist $infolist): Infolist
+    // {
+    //     return $infolist
+    //         ->schema([
+    //             Infolists\Components\Split::make([
+    //                 Infolists\Components\Section::make()
+    //                     ->schema([
+    //                         Infolists\Components\ImageEntry::make('thumbnail'),
+    //                     ]),
+    //                 Infolists\Components\Section::make()
+    //                     ->schema([
+    //                         Infolists\Components\TextEntry::make('title')
+    //                     ])
+    //             ])
+    //         ]);
+    // }
 
     public static function getRelations(): array
     {
@@ -103,6 +126,7 @@ class EventResource extends Resource
             'index' => Pages\ListEvents::route('/'),
             'create' => Pages\CreateEvent::route('/create'),
             'edit' => Pages\EditEvent::route('/{record}/edit'),
+            'view' => Pages\ViewEvent::route('/{record}/detail')
         ];
     }
 }
---
+++ app/Filament/Resources/EventResource/Pages/ViewEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\EventResource\Pages;
+
+use App\Filament\Resources\EventResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewEvent extends ViewRecord
+{
+    protected static string $resource = EventResource::class;
+
+}
