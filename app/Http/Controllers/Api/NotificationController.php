d3a4a4a4a63e692c46089770e3a2bdda44242324
Arifin <dev.arifin14@gmail.com>
2024-07-28 23:12:33 +0800

    transform api notification

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -9,13 +9,20 @@ use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    public function index() : JsonResponse
+    public function index(): JsonResponse
     {
         return response()->json([
             'message' => 'success',
             'data' => Notification::query()
-                        ->take(10)
-                        ->get()
+                ->take(10)
+                ->get()
+                ->transform(fn($item) => [
+                    "id" => $item->id,
+                    "title" => $item->title,
+                    "context" => $item->context,
+                    "is_pin" => $item->is_pin,
+                    "created_at" => $item->created_at->format('d-m-Y'),
+                ])
         ]);
     }
 }
