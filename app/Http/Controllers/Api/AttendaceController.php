74688a951c4654d8447c15d3d60f94b1c0ddfc74
Arifin <dev.arifin14@gmail.com>
2024-08-05 21:10:44 +0800

    update response api attendace 1

--- app/Http/Controllers/Api/AttendaceController.php
+++ app/Http/Controllers/Api/AttendaceController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Attandance;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AttendaceController extends Controller
 {
-    public function index() : JsonResponse
+    public function index(): JsonResponse
     {
         $attendace = Attandance::with('event')
             ->where('user_id', auth()->id())
@@ -19,7 +20,25 @@ class AttendaceController extends Controller
             ->transform(fn($item) => [
                 'id' => $item->id,
                 'event_id' => $item->event->id,
-                'event' => $item->event,
+                'event' => [
+                    "id" => $item->event->id,
+                    "thumbnail" => sprintf("%s/storage/%s", env('APP_URL'), $item->event->thumbnail),
+                    "title" => $item->event->title,
+                    "description" => $item->event->description,
+                    "content" => $item->event->content,
+                    "start_datetime" => Carbon::parse($item->event->start_datetime)->format('d/m/Y'),
+                    "end_datetime" => Carbon::parse($item->event->end_datetime)->format('d/m/Y'),
+                    "is_endedtime" => $item->event->is_endedtime,
+                    "user" => value(fn($user) => [
+                        'id' => $user->id,
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ], $item->event->user),
+                    'attendace_total' => $item->event->attendace->count(),
+                    'is_user_attendace' => auth()->id() ? $item->event->attendace->where('user_id', auth()->id())->first()?->is_present : false,
+                    "created_at" => Carbon::parse($item->event->created_at)->format('d/m/Y'),
+                    "updated_at" => Carbon::parse($item->event->updated_at)->format('d/m/Y')
+                ],
                 'event_start' => \Carbon\Carbon::parse($item->event->start_datetime)->format('d-m-Y H:i'),
                 'end_datetime' => \Carbon\Carbon::parse($item->event->end_datetime)->format('d-m-Y H:i'),
             ]);
